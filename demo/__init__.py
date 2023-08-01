729f5ede073adbd6343564e4de77806c03bd8506
Mistrycoaster <136497273+Mistrycoaster@users.noreply.github.com>
2023-07-31 20:43:45 -0400

    Add files via upload

---
+++ demo/__init__.py
@@ -0,0 +1,1 @@
+print("Welcome to my amazing jabber module that prints out a message")
---
+++ demo/jabber.py
@@ -0,0 +1,4 @@
+def jab():
+    ''' This is my docstring that I am using for the demo today. It will print out a message variable that has been defined.'''
+    print(x)
+    
