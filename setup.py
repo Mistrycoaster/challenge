05abf69d773f54d68435a44ae660a9c2ac4c28bc
Mistrycoaster <136497273+Mistrycoaster@users.noreply.github.com>
2023-07-31 20:44:30 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='demo',
+    version='1.0.0',
+    url='https://github.com/Mistrycoaster/challenge',
+    author='Brandon Mistry',
+    author_email='jqd2hj@virginia.edu',
+    description='Prints out a message provided by the user',
+    license='MIT',
+    packages=['demo'])
